41a69b5cc4da2a84ed5a2dec0af3551cfb66b7d0
Doidel <romanbolzern@hotmail.com>
2018-01-09 14:55:51 +0100

    Trash removed

--- Tools/VideoCreator/VideoCreator/Program.cs
+++ Tools/VideoCreator/VideoCreator/Program.cs
@@ -231,43 +231,7 @@ namespace VideoCreator
             }
         }
 
-        /*
-        /// <summary>
-        /// Resize the image to the specified width and height.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="width">The width to resize to.</param>
-        /// <param name="height">The height to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public static Bitmap ResizeImage(Bitmap image, int width, int height)
-        {
-            // taken from https://stackoverflow.com/questions/1922040/resize-an-image-c-sharp/24199315#24199315
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
-        }
-
-        */
-
-            public static Bitmap NewGrayscale(Bitmap original)
+        public static Bitmap NewGrayscale(Bitmap original)
         {
             // https://stackoverflow.com/questions/12168654/image-processing-with-lockbits-alternative-to-getpixel
             Rectangle rect = new Rectangle(0, 0, original.Width, original.Height);
@@ -305,65 +269,12 @@ namespace VideoCreator
             }
 
             grayBmp.Palette = pal;
-            
+
             // copy generated grayscale pixels into new grayscale bitmap
             Marshal.Copy(bytes, 0, grayPtr, grayBytes);
 
             grayBmp.UnlockBits(grayData);
             return grayBmp;
-
-            /*Bitmap bitmap = new Bitmap(original.Width, original.Height, PixelFormat.Format8bppIndexed);
-            var bitmapData = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadWrite, bitmap.PixelFormat);
-            byte[] buffer = new byte[bitmap.Width * bitmap.Height];
-            //for (int y = 0; y < bitmap.Height; y++)
-            //{
-            //    for (int x = 0; x < bitmap.Width; x++)
-            //    {
-            //        buffer[y * bitmap.Width + x] = original.GetPixel(x, y).R;
-            //    }
-            //}
-            var olb = original.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadOnly, original.PixelFormat);
-            olb.
-            Marshal.Copy(buffer, 0, bitmapData.Scan0, buffer.Length);
-            bitmap.UnlockBits(bitmapData);
-            //bitmap.Save("test.bmp", ImageFormat.Bmp);
-            return bitmap;*/
-        }
-
-        // https://stackoverflow.com/questions/2265910/convert-an-image-to-grayscale
-        public static Bitmap MakeGrayscale3(Bitmap original)
-        {
-            //create a blank bitmap the same size as original
-            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
-
-            //get a graphics object from the new image
-            Graphics g = Graphics.FromImage(newBitmap);
-
-            //create the grayscale ColorMatrix
-            ColorMatrix colorMatrix = new ColorMatrix(
-               new float[][]
-               {
-                 new float[] {.3f, .3f, .3f, 0, 0},
-                 new float[] {.59f, .59f, .59f, 0, 0},
-                 new float[] {.11f, .11f, .11f, 0, 0},
-                 new float[] {0, 0, 0, 1, 0},
-                 new float[] {0, 0, 0, 0, 1}
-               });
-
-            //create some image attributes
-            ImageAttributes attributes = new ImageAttributes();
-
-            //set the color matrix attribute
-            attributes.SetColorMatrix(colorMatrix);
-
-            //draw the original image on the new image
-            //using the grayscale color matrix
-            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
-               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
-
-            //dispose the Graphics object
-            g.Dispose();
-            return newBitmap;
         }
     }
 }
